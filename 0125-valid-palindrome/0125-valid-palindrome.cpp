c6d5b8e593c7d2dcf07835687ed6cd5647f93cc9
FizaLatif6831 <fizalatif6831@gmail.com>
2024-07-29 02:38:10 +0500

    Time: 6 ms (44.99%), Space: 8.9 MB (54.27%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         int left = 0, right = s.length()-1;
+        
         while(left<right)
         {
             if(!isalnum(s[left]))
