bbda5bcffc02d150fad89ee25fb39e22689a3ec1
FizaLatif6831 <fizalatif6831@gmail.com>
2024-10-31 13:18:08 +0500

    Time: 0 ms (100%), Space: 11.3 MB (22.35%) - LeetHub

---
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+        vector<bool> result(candies.size());
+        int max= *max_element(candies.begin(),candies.end());
+
+        for(int i=0;i<candies.size();i++)
+        {
+            if(candies[i]+extraCandies>=max)
+            {
+                result[i]=true;
+            }
+            else
+            {
+                result[i]=false;
+            }
+        }
+
+        return result;
+    }
+};
